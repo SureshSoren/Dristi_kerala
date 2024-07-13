c61118100daf623cb0e9dee40696cf27287f1f2c
shambhavigoel1002 <113904610+shambhavigoel1002@users.noreply.github.com>
2024-07-13 15:10:08 +0530

    Updated Micro UI folder for accepting dpg drop

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/hearings/src/hooks/services/Urls.js
+++
@@ -1,8 +0,0 @@
-export const Urls = {
-  Authenticate: "/user/oauth/token",
-  hearing: {
-    hearingUpdate: "/hearing/v1/update_transcript_additional_attendees",
-    searchHearings: "/hearing/v1/search",
-    updateHearings: "/hearing/v1/update"
-  },
-};
--- frontend/micro-ui/web/micro-ui-internals/packages/modules/submissions/src/hooks/services/Urls.js
+++
@@ -1,9 +0,0 @@
-export const Urls = {
-  Authenticate: "/user/oauth/token",
-  application: {
-    applicationCreate: "/application/v1/create",
-    applicationUpdate: "/application/v1/update",
-    applicationSearch: "/application/v1/search",
-    pendingTask: "/analytics/pending_task/v1/create",
-  },
-};
